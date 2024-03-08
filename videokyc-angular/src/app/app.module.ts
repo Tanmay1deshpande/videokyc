862b3ecf1bea10df80249531e628c9814dd67019
poisonbaba <dtanmay17@gmail.com>
2024-03-08 11:02:38 +0530

    basic components and pages added

--- videokyc-angular/src/app/app.module.ts
+++ videokyc-angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './page/signup/signup.component';
+import { LoginComponent } from './page/login/login.component';
+import { HomeComponent } from './page/home/home.component';
+import { VkycComponent } from './page/vkyc/vkyc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    VkycComponent
   ],
   imports: [
     BrowserModule,
---
+++ videokyc-angular/src/app/page/vkyc/vkyc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vkyc',
+  templateUrl: './vkyc.component.html',
+  styleUrls: ['./vkyc.component.css']
+})
+export class VkycComponent {
+
+}
