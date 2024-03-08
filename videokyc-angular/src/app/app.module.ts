eddc99f395918b1e0a7e6f848f6cc01f61adbc18
poisonbaba <dtanmay17@gmail.com>
2024-03-08 15:21:11 +0530

    Sign up page completed design

--- videokyc-angular/src/app/app.module.ts
+++ videokyc-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
