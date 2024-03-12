9c33969c38820e3dab7a96829f8d12446a90cda2
Sejal Ahire <79636550+SejalAhire@users.noreply.github.com>
2024-03-12 13:24:03 +0530

    Delete src directory

--- src/main/java/com/sample1/firstsp/FormDataRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sample1.firstsp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FormDataRepository extends JpaRepository<FormData, Long> {
-}
