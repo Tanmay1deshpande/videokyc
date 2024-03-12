d8bb54a5842b8770911a15b953b2444f91c6b0e5
sejal ahire <sejalahire.19@gmail.com>
2024-03-12 13:33:18 +0530

    added

---
+++ firstsp/src/main/java/com/sample1/firstsp/FirstspApplication.java
@@ -0,0 +1,13 @@
+package com.sample1.firstsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspApplication.class, args);
+	}
+
+}
