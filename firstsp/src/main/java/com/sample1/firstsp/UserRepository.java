c7a05e610afd33819db08283846cf257ff0a34a0
poisonbaba <dtanmay17@gmail.com>
2024-03-13 10:35:09 +0530

    idk

---
+++ firstsp/src/main/java/com/sample1/firstsp/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sample1.firstsp;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
---
+++ firstsp/src/test/java/com/sample1/firstsp/FirstspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample1.firstsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
